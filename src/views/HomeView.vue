cd67a8563fe36aaa40b333f0c27ff4a8b27a6926
Caio Vinicius <caio.vinnicius2k@gmail.com>
2023-04-28 15:58:33 -0300

    fixing product div pos

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -36,95 +36,109 @@
           class="focus:outline-none focus:ring-slate-100 border border-solid border-gray-100 rounded-lg text-xl h-12 p-2"
           type="search"
           placeholder="Search..."
+          @keyup.enter="searchFilter"
         />
-        <button class="border border-slate-500 h-12 w-12 p-1 rounded-md" @click="searchFilter">
+        <button
+          class="border border-slate-500 h-12 w-12 p-1 rounded-md"
+          @click="searchFilter"
+          :disabled="!searchProduct"
+        >
           <SearchIcon />
         </button>
       </div>
     </div>
   </div>
-  <div class="flex items-start flex-wrap">
-    <div class="ml-20 gap-5 flex">
-      <div class="w-1/5">
-        <h1 class="text-2xl text-gray-600 tracking-widest">Categories</h1>
-        <ul>
-          <li>
-            <button @click="getAllProducts" class="m-2 text-gray-600 hover:text-gray-950">
-              All
-            </button>
-          </li>
-          <li v-for="(category, index) in categoriesList" :key="index">
-            <button @click="filterCategory(category)" class="m-1 text-gray-600 hover:text-gray-950">
-              {{ category }}
-            </button>
-          </li>
-        </ul>
-      </div>
 
-      <div class="">
-        <div v-if="notFound" class="w-full ml-5 bg-slate-50 border border-slate-500">
-          <p class="text-lg text-slate-600">There are no products returned for this search!</p>
-        </div>
-        <section
-          v-if="!notFound"
-          class="grid md:grid-cols-5 pl-16 mr-12 grid-cols-1 border border-slate-500 p-5 bg-slate-50 rounded-lg"
-        >
-          <ListProducts
-            @show-modal="getSingleProduct(product.id)"
-            v-for="product in products"
-            :key="product.id"
-            :title="product.title"
-            :price="product.price"
-            :category="product.category"
-            :thumbnail="product.thumbnail"
-          />
-          <v-dialog v-model="dialog" width="auto">
-            <v-card>
-              <v-dialog v-model="dialog" class="w-3/5">
-                <v-card class="text-gray-600 bg-slate-200 rounded-lg p-4">
-                  <div class="flex gap-10 m-10">
-                    <div class="lg:w-4/5 mx-auto flex flex-wrap">
-                      <img
-                        alt="ecommerce"
-                        class="lg:w-1/2 w-full lg:h-auto h-72 object-cover object-center rounded"
-                        :src="product.thumbnail"
-                      />
-                      <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-                        <h2 class="text-sm title-font text-gray-500 tracking-widest">
-                          {{ product.category }}
-                        </h2>
-                        <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">
-                          {{ product.title }}
-                        </h1>
-                        <h2 class="text-sm title-font text-gray-500 tracking-widest">
-                          {{ product.brand }}
-                        </h2>
-                        <span class="text-gray-600 mt-2">{{ product.rating }} Reviews</span>
-                        <p class="mt-4">
-                          {{ product.description }}
-                        </p>
-                        <div class="flex justify-between items-center mt-4">
-                          <span class="title-font font-medium text-2xl text-gray-900"
-                            >R${{ product.price }},00</span
+  <!-- DIV GERAL  -->
+  <div class="w-full flex">
+    <!-- INICIO DIV DAS CATEGORIAS  -->
+    <div class="w-52 mx-10">
+      <h1 class="text-2xl text-gray-600">Categories</h1>
+      <ul>
+        <li>
+          <button @click="getAllProducts" class="m-2 text-gray-600 hover:text-gray-950">All</button>
+        </li>
+        <li v-for="(category, index) in categoriesList" :key="index">
+          <button @click="filterCategory(category)" class="m-1 text-gray-600 hover:text-gray-950">
+            {{ category }}
+          </button>
+        </li>
+      </ul>
+    </div>
+    <!-- FIM DIV DAS CATEGORIAS  -->
+
+    <!-- DIV DA LISTA DE PRODUTOS -->
+    <div class="w-full">
+      <div
+        v-if="notFound"
+        class="w-full h-full flex flex-col justify-center bg-slate-50 border border-slate-500"
+      >
+        <p class="text-lg text-slate-600 text-center">
+          Não foram encontrados produtos para a pesquisa desejada!
+        </p>
+      </div>
+      <section
+        v-if="!notFound"
+        class="grid md:grid-cols-5 pl-16 mr-12 grid-cols-1 border border-slate-500 p-5 bg-slate-50 rounded-lg"
+      >
+        <ListProducts
+          @show-modal="getSingleProduct(product.id)"
+          v-for="product in products"
+          :key="product.id"
+          :title="product.title"
+          :price="product.price"
+          :category="product.category"
+          :thumbnail="product.thumbnail"
+        />
+        <v-dialog v-model="dialog" width="auto">
+          <v-card>
+            <v-dialog v-model="dialog" class="w-3/5">
+              <v-card class="text-gray-600 bg-slate-200 rounded-lg p-4">
+                <div class="flex gap-10 m-10">
+                  <div class="lg:w-4/5 mx-auto flex flex-wrap">
+                    <img
+                      alt="ecommerce"
+                      class="lg:w-1/2 w-full lg:h-auto h-72 object-cover object-center rounded"
+                      :src="product.thumbnail"
+                    />
+                    <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+                      <h2 class="text-sm title-font text-gray-500 tracking-widest">
+                        {{ product.category }}
+                      </h2>
+                      <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">
+                        {{ product.title }}
+                      </h1>
+                      <h2 class="text-sm title-font text-gray-500 tracking-widest">
+                        {{ product.brand }}
+                      </h2>
+                      <span class="text-gray-600 mt-2">{{ product.rating }} Reviews</span>
+                      <p class="mt-4">
+                        {{ product.description }}
+                      </p>
+                      <div class="flex justify-between items-center mt-4">
+                        <span class="title-font font-medium text-2xl text-gray-900"
+                          >R${{ product.price }},00</span
+                        >
+                        <v-card-actions class="flex justify-center">
+                          <v-btn color="secundary" @click="dialog = false" class="bg-black w-20"
+                            >Close</v-btn
                           >
-                          <v-card-actions class="flex justify-center">
-                            <v-btn color="secundary" @click="dialog = false" class="bg-black w-20"
-                              >Close</v-btn
-                            >
-                          </v-card-actions>
-                        </div>
-                        <span class="text-gray-600 mt-2">{{ product.stock }} stock</span>
+                        </v-card-actions>
                       </div>
+                      <span class="text-gray-600 mt-2">{{ product.stock }} stock</span>
                     </div>
                   </div>
-                </v-card>
-              </v-dialog>
-            </v-card>
-          </v-dialog>
-        </section>
-      </div>
+                </div>
+              </v-card>
+            </v-dialog>
+          </v-card>
+        </v-dialog>
+      </section>
     </div>
+    <!-- FIM DIV DA LISTA DE PRODUTOS -->
   </div>
+  <!-- FIM DIV GERAL  -->
+
   <div>
     <div class="flex justify-center py-10 bg-slate-200 mt-10">
       <p class="font-medium text-center text-gray-800">dummyStore &copy; 2023</p>
